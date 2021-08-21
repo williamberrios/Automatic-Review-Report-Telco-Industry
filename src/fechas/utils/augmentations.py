4044a9441a81695b48309f099a68479caf403cc5
williamberrios <wberriosr@uni.pe>
2021-08-21 22:55:47 +0000

    update william

--- src/fechas/utils/augmentations.py
+++ src/fechas/utils/augmentations.py
@@ -22,6 +22,7 @@ def get_transform(name = 'train',MEAN = None,STD = None):
                     ),
                     ToTensorV2()],bbox_params=A.BboxParams(format='pascal_voc',label_fields=['labels']))
         elif name == 'test':
+            print('test')
             return A.Compose([
                     A.Normalize(
                     mean = MEAN,
